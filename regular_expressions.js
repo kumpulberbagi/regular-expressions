6ef831e89aa2786a92353941438a5dcadcd188b4
broerjuang <juangwiantoro@gmail.com>
2016-10-02 16:13:31 +0700

    Final Submission Regular Expression

--- regular_expressions.js
+++ regular_expressions.js
@@ -1,7 +1,8 @@
 "use strict"
 // Determine whether a string contains a nomor KTP
 function has_ktp(string) {
-
+  var re = /\d+(-\d{2,4})+/g;
+  return re.test(string);
 }
 
 console.log("has_ktp returns true if it has what looks like a nomor KTP")
@@ -12,7 +13,12 @@ console.log(has_ktp("please confirm your identity: XXX-XX-1422") == false)
 
 // Return the Social Security number from a string.
 function grab_ktp(string) {
-
+  let reg = /\d+(-\d{2,4})+/g;
+  if (reg.test(string) === true) {
+    return string.match(reg);
+  } else {
+    return null;
+  }
 }
 
 console.log("grab_ktp returns an nomor KTP if the string has an nomor KTP")
@@ -23,7 +29,12 @@ console.log(grab_ktp("please confirm your identity: XXX-XX-1422") == null)
 
 // Return all of the Social Security numbers from a string.
 function grab_all_nomor_ktp(string) {
-
+  let reg = /\d+(-\d{2,4})+/g;
+  if (reg.test(string) === true) {
+    return string.match(reg);
+  } else {
+    return [];
+  }
 }
 
 console.log("grab_all_nomor_ktp returns all nomor KTP if the string has any nomor KTP")
@@ -38,7 +49,12 @@ console.log(grab_all_nomor_ktp("please confirm your identity: XXX-XX-1422"))
 
 // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
 function hide_all_nomor_ktp(string) {
-
+  let reg = /\d{2,3}-/g;
+  if (reg.test(string) === true) {
+    return string.replace(reg, 'XXX-XX-');
+  } else {
+    return string;
+  }
 }
 
 console.log("hide_all_nomor_ktp obfuscates any nomor KTP in the string")
@@ -54,7 +70,12 @@ console.log(hide_all_nomor_ktp(string) == string)
 // Ensure all of the Social Security numbers use dashes for delimiters.
 // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 function format_nomor(string) {
-
+  let reg= /(\d{3})[\.-]?(\d{2})[\.-]?(\d{4})/g
+  if(reg.test(string) === true) {
+    return string.replace(reg, "$1-$2-$3");
+  } else {
+    return string;
+  }
 }
 
 console.log("format_nomor finds and reformat any nomor KTP in the string")
